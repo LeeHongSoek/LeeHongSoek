5a2468a1c3180a01f9f376d29a8b2753ccd57047
이홍석(GMail) <105229755+LeeHongSoek@users.noreply.github.com>
2023-06-05 15:57:09 +0900

    eeeeeeeee

--- git.py
+++ git.py
@@ -15,6 +15,9 @@ os.chdir(local_repo)
 subprocess.call(["git", "remote", "add", "origin", remote_url])
 
 while True:
+    # 원격 저장소의 변경 사항을 가져옵니다.
+    subprocess.run(["git", "pull", "origin", "main"])
+
     # git add --all
     subprocess.run(["git", "add", "--all", "-u"])
 
@@ -27,7 +30,7 @@ while True:
     # 변경된 파일이 있는 경우 커밋하고 푸시합니다.
     if num_files > 0:
         subprocess.run(["git", "commit", "-m", "Auto-commit"])
-        subprocess.run(["git", "push", "origin", "master"])
+        subprocess.run(["git", "push", "origin", "main"])
 
     # 10초 동안 대기합니다.
     time.sleep(10)
