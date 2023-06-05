658fd750e179e2ed95f35c7515cea5934f7d645e
이홍석(GMail) <105229755+LeeHongSoek@users.noreply.github.com>
2023-06-05 16:10:11 +0900

    Auto-commit

--- git.py
+++ git.py
@@ -15,23 +15,24 @@ os.chdir(local_repo)
 # 원격 저장소를 추가합니다.
 subprocess.call(["git", "remote", "add", "origin", remote_url])
 
-while True:
-    # 원격 저장소의 변경 사항을 가져옵니다.
-    subprocess.run(["git", "pull", "origin", "main"])
+#while True:
 
-    # git add --all
-    subprocess.run(["git", "add", "-u"])
+# 원격 저장소의 변경 사항을 가져옵니다.
+subprocess.run(["git", "pull", "origin", "main"])
 
-    # git status
-    result = subprocess.run(["git", "status"], capture_output=True, text=True)
+# git add --all
+subprocess.run(["git", "add", "-u"])
 
-    # 변경된 파일의 수
-    num_files = len(result.stdout.splitlines()) - 1
+# git status
+result = subprocess.run(["git", "status"], capture_output=True, text=True)
 
-    # 변경된 파일이 있는 경우 커밋하고 푸시합니다.
-    if num_files > 0:
-        subprocess.run(["git", "commit", "-m", "Auto-commit"])
-        subprocess.run(["git", "push", "origin", "main"])
+# 변경된 파일의 수
+num_files = len(result.stdout.splitlines()) - 1
 
-    # 10초 동안 대기합니다.
-    time.sleep(10)
+# 변경된 파일이 있는 경우 커밋하고 푸시합니다.
+if num_files > 0:
+    subprocess.run(["git", "commit", "-m", "Auto-commit"])
+    subprocess.run(["git", "push", "origin", "main"])
+
+# 10초 동안 대기합니다.
+#    time.sleep(10)
